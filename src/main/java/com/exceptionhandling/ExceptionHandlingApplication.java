52a93b2a2c8896bf6bea6c76bddf3ec9c044882f
imprity <imprity041@gmail.com>
2026-01-12 20:28:55 +0900

    first commit

---
+++ src/main/java/com/exceptionhandling/ExceptionHandlingApplication.java
@@ -0,0 +1,127 @@
+package com.exceptionhandling;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@SpringBootApplication
+public class ExceptionHandlingApplication {
+  @Getter
+  public static class Member {
+    @NotBlank(message = "이름이 비어있으면 안됩니다")
+    @Size(min = 2, message = "이름은 {min}글자 이상이어야 합니다.")
+    private String name;
+
+    @Min(value = 18, message = "나이는 {value}이상이어야 합니다.")
+    private int age;
+  }
+
+  @RestController
+  @RequiredArgsConstructor()
+  public static class Controller {
+    private final MemberService memberService;
+
+    @PostMapping("/api/sign-up")
+    public ResponseEntity<Member> signUp(@Valid @RequestBody Member member) {
+      memberService.saveMember(member);
+
+      return ResponseEntity.status(HttpStatus.CREATED).body(member);
+    }
+
+    @GetMapping("/api/members")
+    public ResponseEntity<List<Member>> getMembers() {
+      List<Member> members = memberService.getMembers();
+
+      return ResponseEntity.status(HttpStatus.OK).body(members);
+    }
+
+    @GetMapping("/api/check")
+    public ResponseEntity<SimpleResponse> check(@RequestParam String name) {
+      SimpleResponse res = memberService.check(name);
+      return ResponseEntity.status(HttpStatus.OK).body(res);
+    }
+  }
+
+  @Getter
+  @RequiredArgsConstructor
+  public static class SimpleResponse {
+    private final String msg;
+  }
+
+  public static class SimpleException extends RuntimeException {
+    public SimpleException(String msg) {
+      super(msg);
+    }
+  }
+
+  @Service
+  public static class MemberService {
+    private final List<Member> members = Collections.synchronizedList(new ArrayList<>());
+
+    public void saveMember(Member member) {
+      members.add(member);
+    }
+
+    public List<Member> getMembers() {
+      return new ArrayList<Member>(members);
+    }
+
+    public SimpleResponse check(String name) {
+      for (final Member member : members) {
+        if (member.getName().equals(name)) {
+          throw new SimpleException("이미 회원가입된 이름입니다");
+        }
+      }
+
+      return new SimpleResponse("사용 가능한 이름입니다.");
+    }
+  }
+
+  @RestControllerAdvice
+  @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+  public static class ExceptionHandlerController {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<SimpleResponse> handleValidationError(MethodArgumentNotValidException e) {
+      String msg = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+
+      SimpleResponse res = new SimpleResponse(msg);
+
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
+    }
+
+    @ExceptionHandler(SimpleException.class)
+    public ResponseEntity<SimpleResponse> handleValidationError(SimpleException e) {
+      String msg = e.getMessage();
+
+      SimpleResponse res = new SimpleResponse(msg);
+
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
+    }
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(ExceptionHandlingApplication.class, args);
+  }
+}
